c385ff8dca1bf82644a42317fa2699e748f54769
knt.t <153593861+kntt32@users.noreply.github.com>
2024-09-10 14:03:36 +0000

    ファイルの消去

--- target/debug/main.d
+++
@@ -1,1 +0,0 @@
-/workspaces/SurarueN/target/debug/main: /workspaces/SurarueN/main/src/main.rs /workspaces/SurarueN/main/src/matrix.rs /workspaces/SurarueN/main/src/neuron.rs /workspaces/SurarueN/main/src/rand.rs
--- target/release/main.d
+++
@@ -1,1 +0,0 @@
-/workspaces/SurarueN/target/release/main: /workspaces/SurarueN/main/src/main.rs /workspaces/SurarueN/main/src/matrix.rs /workspaces/SurarueN/main/src/neuron.rs /workspaces/SurarueN/main/src/rand.rs
